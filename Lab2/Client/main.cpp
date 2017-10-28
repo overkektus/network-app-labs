29fde2a46c98be27bdb196e39b699ed984e252ea
Egor Piskunou <egor.piskunou@gmail.com>
2017-10-28 10:11:11 +0300

    done lab2

--- Lab2/Client/main.cpp
+++ Lab2/Client/main.cpp
@@ -8,9 +8,11 @@
 #include "Winsock2.h"
 #include "../Serv/TCPConnection.h"
 #pragma comment(lib, "WS2_32.lib")
+#pragma warning (disable : 4996)
 
 using namespace std;
 
+void prepareString(string& str, int i);
 
 int main()
 {
@@ -18,14 +20,24 @@ int main()
 
 	WSADATA wsa;
 	SOCKET cC;
+	int countMsg;
 
 	try {
+		//----------------------------------------1----------------------------------------
+		//»нициализаци€ WS2_32.DLL
 		if (WSAStartup(MAKEWORD(2, 0), &wsa) != 0)
 			throw TCPConnection::Helper::SetErrorMsgText("Startup: ", WSAGetLastError());
 
+		//----------------------------------------2----------------------------------------
+		//создание сокета и установка его параметров. сокет будет использоватьс€ дл€ соединени€
+		//(сокет, ориентированный на поток)
 		if ((cC = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
 			throw TCPConnection::Helper::SetErrorMsgText("Socket: ", WSAGetLastError());
 
+		//----------------------------------------3----------------------------------------
+		//connect предназначенна€ дл€ установки соединени€ с сокетом сервера.
+		//в качестве параметров имеет созданный в предыдущем блоке дескриптор сокета,
+		//ориентированного на поток, и структуру SOCKADDR_IN с параметрами сокета сервера.
 		SOCKADDR_IN serv;
 		serv.sin_family = AF_INET;
 		serv.sin_port = htons(2000);
@@ -34,17 +46,38 @@ int main()
 		if ((connect(cC, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR)
 			throw TCPConnection::Helper::SetErrorMsgText("Connect: ", WSAGetLastError());
 
+		cout << " оличество сообщений: ";
+		cin >> countMsg; cout << endl;
 
-		char obuf[50] = "Hello from client "; // буфер вывода
+		//----------------------------------------4----------------------------------------
+		//отправка сообщений sendto
 
-		for (int i = 0; i < 1000; i++) {
-			int lobuf = 0; // количество отправленных
-			if ((lobuf = sendto(cC, obuf, strlen(obuf) + 1, NULL, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR)
-				throw TCPConnection::Helper::SetErrorMsgText("Recv: ", WSAGetLastError());
+		char buf[50] = "Hello from Client 000"; // буфер ввода
+		string bufStr;
+		int libuf = 0;                          // количество отправленных байт
+		int lobuf = 0;                          // количество прин€тых байт
+
+		for (int i = 0; i < countMsg; i++) {
+			if ((libuf = send(cC, buf, sizeof(buf), NULL)) == SOCKET_ERROR)
+				throw TCPConnection::Helper::SetErrorMsgText("send:", WSAGetLastError());
+
+			cout << " оличество отправленый байт: " << libuf << endl;
+			cout << "“екст сообшени€: " << buf << endl << endl;
+
+			//---------------------------------------- 5 ----------------------------------------
+			if ((lobuf = recv(cC, buf, sizeof(buf), NULL)) == SOCKET_ERROR)
+				throw TCPConnection::Helper::SetErrorMsgText("recv:", WSAGetLastError());
+
+			cout << " оличество полученых байт сообщени€: " << lobuf << endl;
+			cout << "“екст сообшени€: " << buf << endl << endl;
+
+			bufStr = (string)buf;
+			prepareString(bufStr, i + 1);
+			strcpy(buf, bufStr.c_str());
 		}
-
 		
-		//«акрытие библиотеки
+		//----------------------------------------6----------------------------------------
+		//закрытие сокета и завершени€ работы с библиотекой WS2_32.DLL
 		if (closesocket(cC) == SOCKET_ERROR)
 			throw TCPConnection::Helper::SetErrorMsgText("Closesocket: ", WSAGetLastError());
 		if (WSACleanup() == SOCKET_ERROR)
@@ -56,4 +89,21 @@ int main()
 
 	_getch();
 	return 0;
+}
+
+void prepareString(string& str, int i)
+{
+	str.erase(18);
+	if (i < 10)
+	{
+		str.append("00");
+		str.append(to_string(i));
+	}
+	else if (i < 100)
+	{
+		str.append("0");
+		str.append(to_string(i));
+	}
+	else
+		str.append(to_string(i));
 }
--- Lab2/Serv/main.cpp
+++ Lab2/Serv/main.cpp
@@ -2,68 +2,86 @@
 #include <iostream>
 #include "conio.h"
 #include "TCPConnection.h"
+#pragma warning (disable : 4996)
 
 using namespace std;
 
-
-int main()
-{
+int main() {
 	setlocale(LC_CTYPE, "rus");
 
 	WSADATA wsa;
 	SOCKET sS;
+	string action;
 
 	try {
-		//Открытие библиотеки
+		//----------------------------------------1----------------------------------------
+		//Инициализация WS2_32.DLL
 		if (WSAStartup(MAKEWORD(2, 0), &wsa) != 0)
 			throw TCPConnection::Helper::SetErrorMsgText("Startup: ", WSAGetLastError());
 
+		//----------------------------------------2----------------------------------------
+		//создание сокета и установка его параметров. сокет будет использоваться для соединения
+		//(сокет, ориентированный на поток)
 		if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
 			throw TCPConnection::Helper::SetErrorMsgText("Socket: ", WSAGetLastError());
 
 		SOCKADDR_IN serv;
-		serv.sin_family = AF_INET;
-		serv.sin_addr.s_addr = INADDR_ANY;
-		serv.sin_port = htons(2000);
+		serv.sin_family = AF_INET; //используется IP-адресация
+		serv.sin_addr.s_addr = INADDR_ANY; //любой собственный IP-адрес
+		serv.sin_port = htons(2000); //порт
 
+		//Для установки параметров сокета используется функция bind.
+		//При этом говорят, что сокет связывают с параметрами.
 		if (bind(sS, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR)
 			throw TCPConnection::Helper::SetErrorMsgText("Bind: ", WSAGetLastError());
 
+		//----------------------------------------3----------------------------------------
+		//listen переводит сокет, ориентированный на поток, в состояния прослушивания(открывает доступ к сокету)
+		//и задает некоторые параметры очереди соединений.
 		if (listen(sS, SOMAXCONN) == SOCKET_ERROR)
 			throw TCPConnection::Helper::SetErrorMsgText("Listen: ", WSAGetLastError());
 
-
-		SOCKET cS;
+		SOCKET cS; //сокет, предназначенный для обмена данными с клиентом
 		SOCKADDR_IN clnt;
 		memset(&clnt, 0, sizeof(clnt));
 		int lclnt = sizeof(clnt);
 
-		if ((cS = accept(sS, (sockaddr*)&clnt, &lclnt)) == INVALID_SOCKET)
-			throw TCPConnection::Helper::SetErrorMsgText("Accept: ", WSAGetLastError());
+		cout << "[1] - ожидание клиента " << endl;
+		cout << "[2] - выход " << endl;
+		cin >> action;
+		//accept переводит процесс сервера в состояние ожидания до момента, пока программа клиента не выполнит
+		//функцию connect(установить соединение)
+		while (action == "1") {
+			if ((cS = accept(sS, (sockaddr*)&clnt, &lclnt)) == INVALID_SOCKET)
+				throw TCPConnection::Helper::SetErrorMsgText("Accept: ", WSAGetLastError());
 
+			//----------------------------------------4----------------------------------------
+			//Обмен данными
+			char bfrom[50]; // буфер ввода
+			int lobuf = 0;  // количество принятых байт
+			int libuf = 0;  // количество отправленных байт
+			cout << endl;
+			cout << "IP: " << inet_ntoa(clnt.sin_addr) << endl;
+			cout << "Port: " << ntohs(clnt.sin_port) << endl << endl;
 
-		printf_s("Connect from: %u.%u.%u.%u:%u\n", 
-			(unsigned char)clnt.sin_addr.S_un.S_un_b.s_b1,
-			(unsigned char)clnt.sin_addr.S_un.S_un_b.s_b2,
-			(unsigned char)clnt.sin_addr.S_un.S_un_b.s_b3,
-			(unsigned char)clnt.sin_addr.S_un.S_un_b.s_b4,
-			ntohs(clnt.sin_port));
+			do {
+				if ((lobuf = recv(cS, bfrom, sizeof(bfrom), NULL)) == SOCKET_ERROR)
+					throw TCPConnection::Helper::SetErrorMsgText("recv:", WSAGetLastError());
 
-		char ibuf[50],
-			obuf[50] = "sever: принято ";
-		int libuf = 0,
-			lobuf = 0;
+				//---------------------------------------- 5 ----------------------------------------
+				if ((libuf = send(cS, bfrom, sizeof(bfrom), NULL)) == SOCKET_ERROR)
+					throw TCPConnection::Helper::SetErrorMsgText("send:", WSAGetLastError());
 
-		for (int i = 0; i < 1000; i++) {
-			if ((libuf = recv(cS, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR)
-				throw TCPConnection::Helper::SetErrorMsgText("Recv:", WSAGetLastError());
+			} while (lobuf != 0);
 
-			cout << "Message from client: " << ibuf << endl;
-			//memset(ibuf, 0, sizeof(ibuf));
+			cout << endl;
+			cout << "[1] - ожидание клиента " << endl;
+			cout << "[2] - выход " << endl;
+			cin >> action;
+			system("cls");
 		}
-
-
-		//Закрытие библиотеки
+		//----------------------------------------6----------------------------------------
+		//закрытие сокета и завершения работы с библиотекой WS2_32.DLL
 		if (closesocket(sS) == SOCKET_ERROR)
 			throw TCPConnection::Helper::SetErrorMsgText("Closesocket: ", WSAGetLastError());
 		if (WSACleanup() == SOCKET_ERROR)
@@ -73,6 +91,5 @@ int main()
 		cout << endl << errorMsg;
 	}
 
-	_getch();
 	return 0;
 }
